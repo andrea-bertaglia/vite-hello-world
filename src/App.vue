44ebe50c48a5477172fd50d17fe0dc1f27e5a6d4
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-30 18:14:14 +0200

    feat: component AppBanner with dynamic path

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppBanner from "./components/AppBanner.vue";
 
 export default {
   components: {
     AppTitle,
+    AppBanner
   },
   data() {
     return {};
-  }
-}
+  },
+};
 </script>
 
 <template>
   <AppTitle />
+  <AppBanner />
 </template>
 
 <style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "Foto di una montagna",
+            image: "mountain.jpg",
+        };
+    },
+    methods: {
+        getImagePath(imageName) {
+            return new URL(`../assets/img/${imageName}`, import.meta.url).href
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="banner">
+        <img :src="getImagePath(image)" :alt="`${title}`">
+        <h3>{{ title }}</h3>
+    </div>
+</template>
+
+<style>
+.banner {
+    border: 5px dashed green;
+    margin-top: 10px;
+    max-width: 500px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1em;
+    padding: 1em;
+    margin: 0 auto;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.banner img {
+    max-width: 100%;
+    
+}
+</style>
