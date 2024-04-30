443632ce85657ac8d0d9c41b5c06484eafe3399b
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-30 17:45:31 +0200

    feat: component AppTitle - creation and insert in page

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            text: "La mia prima app con Vite!"
+        }
+    },
+}
+</script>
+
+<template>
+    <h2 class="title">{{ text }}</h2>
+</template>
+
+<style>
+.title {
+  background-color: lightseagreen;
+  color: greenyellow;
+  font-size: 2rem;
+  padding: 1em;
+  text-align: center;
+}
+</style>
